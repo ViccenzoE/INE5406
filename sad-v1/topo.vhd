5ef22cde7489239ef7ad888291fecef3b344d2f9
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2024-06-01 11:04:57 -0300

    Alterações no testbench

--- sad-v1/topo.vhd
+++ sad-v1/topo.vhd
@@ -1,59 +1,75 @@
-LIBRARY IEEE;
-USE IEEE.Std_Logic_1164.all;
-USE IEEE.std_logic_arith.all;
-USE IEEE.std_logic_unsigned.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.std_logic_textio.all;
+use std.textio.all;
 
-ENTITY Topo IS
-PORT(
-	-- Entradas
-	CLOCK: IN std_logic;
-	iniciar: IN std_logic;
-	reset: IN std_logic;
-	sample_ori: IN std_LOGIC_VECTOR(7 DOWNTO 0);
-	sample_can:IN std_LOGIC_VECTOR(7 DOWNTO 0);
-	
-	-- Saí­das
-	SAD_saida: OUT std_logic_vector(13 DOWNTO 0);
-	end_sad: OUT std_logic_vector(5 DOWNTO 0);
-	read_sad: OUT std_logic;
-	pronto: OUT std_logic
-	
-);
-END ENTITY;
+entity testbench is
+end testbench;
 
-ARCHITECTURE arc OF Topo IS
+architecture tb of testbench is
 
-COMPONENT sad_bo IS
-	PORT(
-		CLK, zi, ci, cpA, cpB, zsoma, csoma, csad_reg: IN STD_LOGIC; -- Sinais de controle
-		sample_ori, sample_can : IN STD_LOGIC_VECTOR(7 downto 0); -- Acesso à memória, ori = A, can = B
-		menor : OUT STD_LOGIC; -- Comparação para identificação da última linha
-		endi : OUT STD_LOGIC_VECTOR(5 downto 0); -- Endereço a ser acessado pela memória
-		SAD : OUT STD_LOGIC_VECTOR(13 downto 0) -- Valor do SAD atual
-	);
-END COMPONENT;
+    signal CLOCK: std_logic := '0';
+    signal iniciar: std_logic := '1';
+    signal reset: std_logic := '0';
+    signal sample_ori: std_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
+    signal sample_can: std_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
 
-COMPONENT sad_bc IS
-	PORT (
-		clk : IN STD_LOGIC; -- ck
-		enable : IN STD_LOGIC; -- iniciar
-		reset : IN STD_LOGIC; -- reset
-		menor : IN STD_LOGIC; -- menor que a quantia de linhas
-		read_mem : OUT STD_LOGIC; -- read
-		done : OUT STD_LOGIC;
-		cpA, cpB, ci, zi, zsoma, csoma, csad_reg : OUT STD_LOGIC
-		);
-END COMPONENT;
+    -- Saída
+    signal SAD_saida: std_logic_vector(13 DOWNTO 0);
+    signal end_sad: std_logic_vector(5 DOWNTO 0);
+    signal read_sad: std_logic;
+    signal pronto: std_logic;
 
-SIGNAL si1, si2, si3, si4, si5, si6, si7, men : std_logic;
+    constant passo: TIME := 20 ns;
 
+begin 
 
-BEGIN 
-	--clock <= CLOCK_50;
-	--clock <= CLOCK_16000000;
-	V1: sad_bo PORT MAP(CLOCK, si1, si2, si3, si4, si5, si6, si7, sample_ori(7 downto 0), sample_can(7 downto 0), men, end_sad, SAD_saida);
-	V2: sad_bc PORT MAP(CLOCK, iniciar, reset, men, read_sad, pronto, si3, si4, si2, si1, si5, si6, si7);
-	
-	
-END ARCHITECTURE;
-	
+    -- Instanciação do módulo Topo
+    DUV: entity work.Topo
+        port map (
+            CLOCK      => CLOCK,
+            iniciar    => iniciar,
+            reset      => reset,
+            sample_ori => sample_ori,
+            sample_can => sample_can,
+            SAD_saida  => SAD_saida,
+            end_sad    => end_sad,
+            read_sad   => read_sad,
+            pronto     => pronto
+        );
+
+    -- Processo para geração do clock
+    clk_process: process
+    begin
+        while True loop
+            CLOCK <= '0';
+            wait for passo / 2;
+            CLOCK <= '1';
+            wait for passo / 2;
+        end loop;
+    end process;
+
+    -- Processo para fornecer estímulos
+    stimulus_process: process
+    begin
+        -- Inicialização dos sinais
+        reset <= '1';
+        wait for passo;
+        reset <= '0';
+
+        sample_ori <= std_LOGIC_VECTOR(to_unsigned(0, sample_ori'length));
+        sample_can <= std_LOGIC_VECTOR(to_unsigned(0, sample_can'length));
+        wait for passo;
+
+        -- Teste 1
+        assert (SAD_saida = "00000000000000")
+        report "Fail 0" severity error;
+        wait for passo;
+		  
+		  
+
+        wait;
+    end process;
+
+end tb;
