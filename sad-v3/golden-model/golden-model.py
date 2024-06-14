df28805ffb035beb886566aca0a34f83d97c8716
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2024-06-13 21:21:07 -0300

    Golden-model refeito

--- sad-v3/golden-model/golden-model.py
+++ sad-v3/golden-model/golden-model.py
@@ -3,31 +3,39 @@ import random
 def generate_binary_32():
     return format(random.randint(0, 2**32 - 1), '032b')
 
-# Gerar números binários de 32 bits
-input_binary = generate_binary_32()
-output_binary = generate_binary_32()
+while True:
+    input_binary = generate_binary_32()
+    output_binary = generate_binary_32()
+    if int(input_binary, 2) > int(output_binary, 2):
+        break
 
 # Separar as strings binárias em segmentos de 8 bits
-input_segments = [input_binary[i:i+8] for i in range(0, 32, 8)]
-output_segments = [output_binary[i:i+8] for i in range(0, 32, 8)]
+entrada11 = input_binary[0:8]
+entrada12 = input_binary[8:16]
+entrada13 = input_binary[16:24]
+entrada14 = input_binary[24:32]
+
+entrada21 = output_binary[0:8]
+entrada22 = output_binary[8:16]
+entrada23 = output_binary[16:24]
+entrada24 = output_binary[24:32]
 
 # Inicializar o acumulador com 0
 accumulator = 0
 
 # Calcular a diferença acumulada entre os segmentos
-for i in range(4):
-    diff = int(input_segments[i], 2) - int(output_segments[i], 2)
-    accumulator += diff
+diff1 = abs(int(entrada11, 2) - int(entrada21, 2))
+diff2 = abs(int(entrada12, 2) - int(entrada22, 2))
+diff3 = abs(int(entrada13, 2) - int(entrada23, 2))
+diff4 = abs(int(entrada14, 2) - int(entrada24, 2))
 
-    # Limitar o acumulador a 14 bits para evitar overflow
-    accumulator &= 0x3FFF  # 0x3FFF é o número 14 bits com todos os bits definidos
+accumulator = diff1 + diff2 + diff3 + diff4
 
-# Converter o acumulador de volta para binário com 14 bits
-accumulator_binary = format(accumulator, '014b')
+accumulator_binary = bin(accumulator)[2:].zfill(14)
 
 # Montar a saída formatada
 output = f"{input_binary} {output_binary} {accumulator_binary}"
 
 # Salvar a saída em um arquivo .dat
 with open('estimulos.dat', 'w') as file:
-    file.write(output)
+   file.write(output)
